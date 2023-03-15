26d1c739dccea37763593291027714c1b5574e70
AbhishekSinghkirola <studyabhi82662@gmail.com>
2023-03-15 21:29:42 +0530

    Innitial Commit

---
+++ script.js
@@ -0,0 +1,70 @@
+// Values and variables
+
+//let firstName = "Ram";
+
+/*
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+*/
+
+//Data types
+
+// primitive , Object
+
+//Number - decimal, integers
+//Strings
+//Boolean
+//undefined
+//null
+//symbol
+//bigint
+
+// let x = -2.6;
+// let y = 'Abhishek';
+// let z = true;
+// let w;
+// let e = null;
+
+// let firstNamePersonValue = "Abhishek";
+
+// let $firstname
+// let _firstname
+// let first5name4
+//let first_name
+// let FirstName
+// let FIRSTNAME
+
+// typeof
+
+// let firstName = "Ram";
+// let x;
+// console.log(typeof "Abhishek");
+
+// console.log(typeof firstName)
+// console.log(typeof 9)
+// console.log(typeof true)
+// console.log(typeof x)
+// console.log(typeof null)
+// let myPreviousJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+// let job1 = "Programmer";
+// let job2 = "Teacher";
+
+//Let Var Const
+
+// let firstName = "Abhishek";
+// firstName = "Ram";
+
+// const PI = 3.1415;
+// const birthYear = 2001;
+
+// let a,b;
+
+// var firstName = "Ram";
+// var firstName = "Rohit";
+
+// firstName = "Ram";
+// console.log(firstName)
