16348ad738b2194ffe1dc024de43bbcb17db9672
AbhishekSinghkirola <studyabhi82662@gmail.com>
2023-03-16 21:28:04 +0530

    Operators and operator precedence

--- script.js
+++ script.js
@@ -68,3 +68,72 @@ console.log(firstName)
 
 // firstName = "Ram";
 // console.log(firstName)
+
+// let a = 5;
+// let b = 6;
+
+// let c = a; //c =5, a=5, b=6
+// a = b; //c= 5, a= 6, b=6
+// b = c;
+
+// console.log("value of a", a);
+// console.log("value of b", b);
+
+// Hello
+// let a = 5;
+// a = "Abhishek"
+// console.log(a)
+
+// Dynamic Typing
+
+//Basic Operators in JS
+// Arithmatic Operators
+// console.log(4 + 5 + 6);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2); //Modulus
+
+// const currentYear = 2023;
+// const ramBirthYear = 2000;
+// const ramAge = currentYear - 2000;
+// console.log(ramAge)
+
+// const firstName = "Rohit";
+// const lastName = "Rathore";
+
+// console.log(firstName + " " + lastName)
+
+//Assignment Operator
+
+// let x = 2;
+// let y ;
+// y = x;
+// console.log(y)
+// x += 2; // x = x+2 = 4
+// x -= 2; // x = x -2 =0
+// x *= 4;
+// x /= 3;
+// x %= 2;
+// x **= 3
+
+// console.log(x) // 2 x 2 x 2 = 8
+
+// Comparison Operator
+
+// const rohitAge = 20;
+// const ramAge = 25;
+// const isTrue = rohitAge >= ramAge;
+// console.log(isTrue) // 20 > 25
+
+// let x = 10 + 5;
+// console.log(x)
+
+// const ageAverage = (rohitAge + ramAge) / 2;
+// console.log(ageAverage)
+
+// ram, ajay 
+//ram w = 95kg , h =1.88m
+//ajay w = 85kg , h =1.68m
+// BMI = w / (height * height) or w / height ** 2
+//rem > ajay --true
