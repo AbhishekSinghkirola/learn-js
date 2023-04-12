7e6a96701ea69af95a2f5bd538ecd68d62d7e5fb
AbhishekSinghkirola <studyabhi82662@gmail.com>
2023-04-12 21:37:12 +0530

    Js Engine

---
+++ script3.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function square(n) {
+  return n * n;
+}
+
+var a = 4;
+// console.log(square(2));
+// console.log(square(a));
+
+console.log(a)
+// function a() {
+//   function b() {
+//     console.log('Function B');
+//   }
+//   b();
+// }
+
+// a();
